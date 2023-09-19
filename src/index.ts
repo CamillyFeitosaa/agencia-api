ab37f4241e6502f729afb4e5699b1e23561e2559
Camilly Vitória Feitosa <euuucamilly18feitosa@gmail.com>
2023-09-19 11:19:37 -0300

    cadastro de pessoas

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,28 @@ const router = Router()
 app.use(express.json()) // Configurando para receber JSON
 app.use(router)
 
+interface Pessoa {
+  nome: string
+  email: string
+}
+const listPessoas: Pessoa[] = []
+
+// GET, POSH, PUT, DELETE, PATH
 router.get('/', (request, response) =>
   response.json({ ola: 'Ola mundo', nome: 'Glêsio' }))
+
+router.post('/formulario', (request, response) => {
+  // const { nome, email } = request.body
+  listPessoas.push({ nome: request.body.nome, email: request.body.email })
+  return response.json().status(201)
+})
+
+router.get('/carregar', (request, response) => {
+  listPessoas.push({ nome: 'Lucas Souza', email: 'lucasSouza@gmail.com' })
+  listPessoas.push({ nome: 'Aysha Cristina', email: 'Ayshacristina@gmail.com' })
+  listPessoas.push({ nome: 'Camilly Vitória', email: 'Camillyvitoria@gmail.com' })
+
+  return response.json({ list: listPessoas })
+})
 
 app.listen(3000, () => { console.log('Running port 3000') })
